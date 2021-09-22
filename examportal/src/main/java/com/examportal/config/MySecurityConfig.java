a047bd3a17b9a2c4aaecc842a622d168f56fcf71
turab45 <turabbajeer202@gmail.com>
2021-09-22 10:10:44 +0500

    add jwt authentication

---
+++ examportal/src/main/java/com/examportal/config/MySecurityConfig.java
@@ -0,0 +1,70 @@
+package com.examportal.config;
+
+import com.examportal.services.impl.CustomUserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomUserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private JwtAuthenticationEntryPoint unAuthorizedHandler;
+    @Autowired
+    private JwtAuthenticationFilter authenticationFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       
+       http.csrf().disable()
+       .cors().disable()
+       .authorizeRequests().antMatchers("/authenticate","/user/").permitAll()
+       .antMatchers(HttpMethod.OPTIONS).permitAll()
+       .anyRequest().authenticated()
+       .and()
+       .exceptionHandling().authenticationEntryPoint(unAuthorizedHandler)
+       .and()
+       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+       
+
+        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
--- examportal/src/main/java/com/examportal/entity/UserRole.java
+++ examportal/src/main/java/com/examportal/entity/UserRole.java
@@ -1,5 +1,7 @@
 package com.examportal.entity;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 
 @Entity
